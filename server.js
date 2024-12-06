be5233aa569da6b40869642141fb55cb79ada14c
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-12-06 14:58:59 +0500

    create user registeration using graphql with node js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js
 const { ApolloServer } = require('apollo-server');
+const express = require('express');
 const connectDB = require('./db/index'); // Import the DB connection function
 const typeDefs = require('./typeDefs/userTypeDefs'); // Import the GraphQL type definitions
 const resolvers = require('./resolvers/userResolver'); // Import the resolvers
@@ -7,6 +8,12 @@ const resolvers = require('./resolvers/userResolver'); // Import the resolvers
 // Connect to MongoDB
 connectDB();
 
+const app = express();
+
+// Root endpoint to verify GraphQL connection
+app.get('/', (req, res) => {
+  res.send('GraphQL server is running and connected!');
+});
 // Set up Apollo Server with type definitions and resolvers
 const server = new ApolloServer({
   typeDefs,
